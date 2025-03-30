053443528e2174fa4a446468b86d53310e8dc6c0
nikola <nnesic172@gmail.com>
2025-03-30 15:27:40 +0200

    fix: final connection string issue fixed

--- Services/Post/Post.Infrastructure/Persistance/DesignTimeFactory.cs
+++ Services/Post/Post.Infrastructure/Persistance/DesignTimeFactory.cs
@@ -12,7 +12,7 @@ namespace Post.Infrastructure.Persistance
         public PostContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder<PostContext>();
-            builder.UseSqlServer("Server=localhost;Database=PostDb;User Id=sa;Password=MATF12345678rs2;TrustServerCertificate=True;Encrypt=False;");
+            builder.UseSqlServer("Server=localhost,8091;Database=PostDb;User Id=sa;Password=MATF12345678rs2;TrustServerCertificate=True;Encrypt=False;");
             return new PostContext(builder.Options);
         }
     }
