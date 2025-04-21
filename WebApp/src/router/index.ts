21a8110e8984914f05c790846bb8f592fad4476f
neske99 <nnesic172@gmail.com>
2025-04-21 15:35:39 +0200

    feat: Added vuetify

--- WebApp/src/router/index.ts
+++ WebApp/src/router/index.ts
@@ -1,53 +1,57 @@
 import { createRouter, createWebHistory, type RouteRecord, type RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import  SignupView  from '@/views/auth/SignupView.vue'
+import SignupView from '@/views/auth/SignupView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import { authStore } from '@/stores/auth'
+import PostsView from '@/views/PostsView.vue'
+import UserDetailView from '@/views/UserDetailView.vue'
+import UserSearchView from '@/views/UserSearchView.vue'
 
 const routes: RouteRecordRaw[] = [
-    {
-      path: '/',
-      name:'root',
-      component:{template:'<div></div>'},
-      beforeEnter: (to, from , next)=>{
-        const auth = authStore();
-        if (auth.isUserAuthenticated) {
-          next('/home')
-        } else {
-          next('/auth/login')
-        }
+  {
+    path: '/',
+    name: 'root',
+    component: { template: '<div></div>' },
+    beforeEnter: (to, from, next) => {
+      const auth = authStore();
+      if (auth.isUserAuthenticated) {
+        next('/home')
+      } else {
+        next('/auth/login')
       }
-    },    
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/auth/signup',
-      name: 'signup',
-      component: SignupView,
-    },
-    {
-      path: '/auth/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: NotFoundView
     }
-  ]
+  },
+  {
+    path: '/home',
+    name: 'posts',
+    component: PostsView,
+  },
+  {
+    path: '/userDetail',
+    name: 'userDetail',
+    component: UserDetailView,
+  },
+  {
+    path: '/userSearch',
+    name: 'userSearch',
+    component: UserSearchView,
+  },
+  {
+    path: '/auth/signup',
+    name: 'signup',
+    component: SignupView,
+  },
+  {
+    path: '/auth/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundView
+  }
+]
 
 
 const router = createRouter({
