59469cf346f39a2c7ae5dc0685a28642b5c1e5e6
pantelic-dusan <pantelic.dusan@protonmail.com>
2025-05-21 08:48:46 +0200

    Complete API implementation

--- Services/Relations/Relations.API/Controllers/RelationsController.cs
+++ Services/Relations/Relations.API/Controllers/RelationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Relations.API.Entities;
 using Relations.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Relations.API.Controllers
 {
@@ -14,12 +15,49 @@ namespace Relations.API.Controllers
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
 
+        [HttpGet("users")]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var friends = await _repository.GetUsers();
+            return Ok(friends);
+        }
+
+        [HttpPost("users")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+        {
+            var result = await _repository.CreateUser(user);
+            return Ok(result);
+        }
+
+        [HttpDelete("users")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> DeleteUser([FromBody] string userId)
+        {
+            var result = await _repository.DeleteUser(userId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
         [HttpGet("friends/{userId}")]
         [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<User>>> GetUserFriends(string userId)
         {
             var friends = await _repository.GetUserFriends(userId);
             return Ok(friends);
+        }
+
+        [HttpDelete("friends/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<User>>> RemoveFriendship(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.RemoveFriendship(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
         }
 
         [HttpGet("blocked/{userId}")]
@@ -30,6 +68,29 @@ namespace Relations.API.Controllers
             return Ok(blockedUsers);
         }
 
+        [HttpPut("blocked/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> BlockUser(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.BlockUser(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+        [HttpDelete("blocked/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> UnblockUser(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.UnblockUser(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+
         [HttpGet("sent/{userId}")]
         [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<User>>> GetSentFriendRequests(string userId)
@@ -38,6 +99,30 @@ namespace Relations.API.Controllers
             return Ok(sentFriendRequests);
         }
 
+        [HttpPut("sent/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> SendFriendRequest(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.SendFriendRequest(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+        [HttpDelete("sent/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> UnsendFriendRequest(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.UnsendFriendRequest(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+
+
         [HttpGet("received/{userId}")]
         [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<User>>> GetReceivedFriendRequests(string userId)
@@ -45,6 +130,30 @@ namespace Relations.API.Controllers
             var receivedFriendRequests = await _repository.GetReceivedFriendRequests(userId);
             return Ok(receivedFriendRequests);
         }
+
+        [HttpPut("received/accept/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> AcceptFriendRequest(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.AcceptFriendRequest(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+        [HttpDelete("received/decline/{sourceUserId}/{targetUserId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> DeclineFriendRequest(string sourceUserId, string targetUserId)
+        {
+            var result = await _repository.DeclineFriendRequest(sourceUserId, targetUserId);
+            if (!result)
+                return NotFound(null);
+            return Ok();
+        }
+
+
 
 
 
--- Services/Relations/Relations.API/Repositories/RelationsRepository.cs
+++ Services/Relations/Relations.API/Repositories/RelationsRepository.cs
@@ -12,11 +12,44 @@ namespace Relations.API.Repositories
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            return await _context.DatabaseClient.Cypher
+                .OptionalMatch("(user:User)")
+                .Return(user => user.As<User>())
+                .ResultsAsync;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                .Create("(user:User $newUser)")
+                .WithParam("newUser", user)
+                .ExecuteWithoutResultsAsync();
+
+            return user;
+        }
+          
+
+        public async Task<Boolean> DeleteUser(string userId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                 .Match("(user:User)-[r]-()")
+                    .Where((User user) => user.Id == userId)
+                    .Delete("r, user")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
         public async Task<IEnumerable<User>> GetUserFriends(string userId)
         {
             await _context.DatabaseClient.ConnectAsync();
             return await _context.DatabaseClient.Cypher
-                .OptionalMatch("(user:User)-[:FRIENDS_WITH]->(friend:User)")
+                .OptionalMatch("(user:User)-[:FRIEND_WITH]->(friend:User)")
                 .Where((User user) => user.Id == userId)
                 .Return(friend => friend.As<User>())
                 .ResultsAsync;
@@ -32,6 +65,51 @@ namespace Relations.API.Repositories
                 .ResultsAsync;
         }
 
+        public async Task<bool> RemoveFriendship(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)-[r:FRIEND_WITH]-(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+        public async Task<bool> BlockUser(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)-[r]-(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)", "(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Create("(sourceUser)-[:BLOCKED]->(targetUser)")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
+        public async Task<bool> UnblockUser(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)-[r:BLOCKED]->(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
         public async Task<IEnumerable<User>> GetSentFriendRequests(string userId)
         {
             await _context.DatabaseClient.ConnectAsync();
@@ -40,6 +118,32 @@ namespace Relations.API.Repositories
                 .Where((User user) => user.Id == userId)
                 .Return(potenital_friend => potenital_friend.As<User>())
                 .ResultsAsync;
+        }
+
+        public async Task<bool> SendFriendRequest(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)", "(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Merge("(sourceUser)-[:REQUESTED_FRIENDSHIP_WITH]->(targetUser)")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
+        public async Task<bool> UnsendFriendRequest(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)-[r:REQUESTED_FRIENDSHIP_WITH]->(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
         }
 
         public async Task<IEnumerable<User>> GetReceivedFriendRequests(string userId)
@@ -51,5 +155,35 @@ namespace Relations.API.Repositories
                 .Return(user => user.As<User>())
                 .ResultsAsync;
         }
+
+        public async Task<bool> AcceptFriendRequest(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)<-[r:REQUESTED_FRIENDSHIP_WITH]-(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Merge("(sourceUser)-[:FRIEND_WITH]->(targetUser)")
+                    .Merge("(sourceUser)<-[:FRIEND_WITH]-(targetUser)")
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
+        public async Task<bool> DeclineFriendRequest(string sourceUserId, string targetUserId)
+        {
+            await _context.DatabaseClient.ConnectAsync();
+            await _context.DatabaseClient.Cypher
+                    .Match("(sourceUser:User)<-[r:REQUESTED_FRIENDSHIP_WITH]-(targetUser:User)")
+                    .Where((User sourceUser) => sourceUser.Id == sourceUserId)
+                    .AndWhere((User targetUser) => targetUser.Id == targetUserId)
+                    .Delete("r")
+                    .ExecuteWithoutResultsAsync();
+
+            return true;
+        }
+
+
     }
 }
